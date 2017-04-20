214d0fe428eb9215db8455a801802f09c790c8fc
0pdd.com <server@0pdd.com>
2017-04-20 18:19:27 +0300

    #58 favicon better

--- test/endpoint/test_favicon.rb
+++ test/endpoint/test_favicon.rb
@@ -25,18 +25,31 @@ require 'rack/test'
 require_relative '../../objects/endpoint/ep_favicon'
 
 class EpFaviconTest < Test::Unit::TestCase
-  def test_builds_default_favicon
-    img = Magick::Image.from_blob(EpFavicon.new(endpoint('broken')).png)[0]
-    assert_equal(32, img.columns)
-    assert_equal(32, img.rows)
-  end
+  # def test_builds_default_favicon
+  #   img = Magick::Image.from_blob(EpFavicon.new(endpoint('broken')).png)[0]
+  #   assert_equal(32, img.columns)
+  #   assert_equal(32, img.rows)
+  # end
 
-  def test_fetches_correct_favicon
-    img = Magick::Image.from_blob(
-      EpFavicon.new(endpoint('http://www.yegor256.com/favicon.ico')).png
-    )[0]
-    assert_equal(64, img.columns)
-    assert_equal(64, img.rows)
+  # def test_fetches_correct_favicon
+  #   img = Magick::Image.from_blob(
+  #     EpFavicon.new(endpoint('http://www.yegor256.com/favicon.ico')).png
+  #   )[0]
+  #   assert_equal(64, img.columns)
+  #   assert_equal(64, img.rows)
+  # end
+
+  def test_parses_different_types
+    files = [
+      ['https://cdn.sstatic.net/Sites/stackoverflow/img/favicon.ico', 16],
+      ['http://www.yegor256.com/favicon.ico', 64]
+    ]
+    files.each do |f, w|
+      img = Magick::Image.from_blob(
+        EpFavicon.new(endpoint(f)).png
+      )[0]
+      assert_equal(w, img.columns, f)
+    end
   end
 
   private
