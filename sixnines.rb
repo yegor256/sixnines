fc7045e1b039bac8260ed0034f67fb578b408b9f
yegor256 <yegor256@gmail.com>
2017-07-19 13:42:01 +0300

    #74 timeout

--- sixnines.rb
+++ sixnines.rb
@@ -20,6 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+require 'timeout'
 require 'haml'
 require 'haml/template/options'
 require 'sinatra'
