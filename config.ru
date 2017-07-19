70637648a94a38d1921d0044c71b19dfe3e66cbf
yegor256 <yegor256@gmail.com>
2017-07-19 13:44:43 +0300

    #74 require_relative

--- config.ru
+++ config.ru
@@ -20,7 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-require './sixnines'
+require_relative 'sixnines'
 
 $stdout.sync = true
 
--- sixnines.rb
+++ sixnines.rb
@@ -20,7 +20,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-require 'timeout'
 require 'haml'
 require 'haml/template/options'
 require 'sinatra'
