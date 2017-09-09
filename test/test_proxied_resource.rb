f8cd8f8ebfd9f32867e5eb96415455e18db55539
Yegor Bugayenko <yegor256@gmail.com>
2017-09-09 08:31:24 +0300

    #91 more

--- test/test_proxied_resource.rb
+++ test/test_proxied_resource.rb
@@ -28,13 +28,14 @@ require_relative '../objects/proxied_resource'
 class ProxiedResourceTest < Test::Unit::TestCase
   def test_pings_valid_uri
     port = FakeServer.new.start(200)
+    proxy = FakeServer.new.start(200)
     assert_equal(
       200,
       ProxiedResource.new(
         Resource.new(URI.parse("http://127.0.0.1:#{port}/")),
         [
           '',
-          '216.230.229.34:60099'
+          "127.0.0.1:#{proxy}"
         ]
       ).take[0]
     )
@@ -51,11 +52,12 @@ class ProxiedResourceTest < Test::Unit::TestCase
   end
 
   def test_pings_invalid_uri
+    proxy = FakeServer.new.start(500)
     assert_not_equal(
       200,
       ProxiedResource.new(
         Resource.new(URI.parse('http://www.definitely-invalid-url-yegor.com')),
-        ['216.230.229.34:60099']
+        ["127.0.0.1:#{proxy}"]
       ).take[0]
     )
   end
