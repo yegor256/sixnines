c2d461aae17cebedd466c46643df9204bb052882
Yegor Bugayenko <yegor256@gmail.com>
2026-01-01 13:09:55 +0300

    year up to 2026

--- objects/exec.rb
+++ objects/exec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# SPDX-FileCopyrightText: Copyright (c) 2017-2025 Yegor Bugayenko
+# SPDX-FileCopyrightText: Copyright (c) 2017-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
 require 'English'
